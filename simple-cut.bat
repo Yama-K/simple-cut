4a463571d47044d00b4b799c0ddfa5a2deabfc59
YamaKoira <26037681+Yama-K@users.noreply.github.com>
2024-01-13 20:44:30 +0200

    Automatic renaming if output file already exists
    Self explanatory

--- simple-cut.bat
+++ simple-cut.bat
@@ -4,6 +4,15 @@ setlocal enabledelayedexpansion
 set input=%1
 set start_time=%2
 set end_time=%3
-set output=!input:.mp4=_cut.mp4!
+set base_output=!input:.mp4=_cut!
+set output=!base_output!.mp4
+set counter=1
+
+:CheckExistence
+if exist !output! (
+    set output=!base_output!-!counter!.mp4
+    set /A counter+=1
+    goto CheckExistence
+)
 
 ffmpeg -i !input! -ss !start_time! -to !end_time! -c copy !output!
