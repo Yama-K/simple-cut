fbfa6a99e8db04ce7afda5255838199a5491960b
YamaKoira <26037681+Yama-K@users.noreply.github.com>
2024-05-04 02:02:05 +0300

    Update simple-cut.bat
    Fixed lag at the beginning of the video, no more discord compression though

--- simple-cut.bat
+++ simple-cut.bat
@@ -1,18 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set input=%1
+rem Get input file path
+set input="%~1"
+
+rem Get start and end times
 set start_time=%2
 set end_time=%3
-set base_output=!input:.mp4=_cut!
-set output=!base_output!.mp4
+
+rem Construct base output file path
+set base_output="%~dpn1_cut.mp4"
+
+rem Initialize counter for handling existing output files
 set counter=1
 
 :CheckExistence
-if exist !output! (
-    set output=!base_output!-!counter!.mp4
+rem Check if the output file already exists; if so, increment counter
+if exist !base_output! (
+    set base_output="%~dpn1_cut-!counter!.mp4"
     set /A counter+=1
     goto CheckExistence
 )
 
-ffmpeg -i !input! -ss !start_time! -to !end_time! -c copy !output!
+rem Trim the video using FFmpeg with the avoid_negative_ts make_zero option
+ffmpeg -i !input! -ss !start_time! -avoid_negative_ts make_zero -to !end_time! -c copy !base_output!
